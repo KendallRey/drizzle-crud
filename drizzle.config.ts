24b255090d7c010b49d34d729240024f96d7f827
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-09-08 20:01:05 +0800

    added initial config
    - drizzle config
    - prettier

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, Config } from 'drizzle-kit';
+
+const config: Config = {
+  schema: "./src/drizzle/schema.ts",
+  out: './src/drizzle/migrations',
+  driver: "pglite",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+  dialect: 'postgresql',
+  verbose: true,
+  strict: true,
+}
+
+export default defineConfig(config)

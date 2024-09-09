3bc7c7ce34eefb766e001b1cbee62955c4765bcb
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-09-09 11:15:32 +0800

    added sample queries
    - update
    - delete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { db } from "./drizzle/db";
 import { UserPreferenceTable, UserTable } from "./drizzle/schema";
-import { sql } from "drizzle-orm";
+import { asc, count, eq, gt, sql } from "drizzle-orm";
 
 const sampleInsert = async () => {
   // await db.delete(UserTable);
@@ -36,21 +36,76 @@ const sampleInsert = async () => {
   console.log(user);
 };
 
+const sampleUpdate = async () => {
+  const user = await db
+    .update(UserTable)
+    .set({
+      age: 30,
+    })
+    .where(eq(UserTable.age, 26))
+    .returning({
+      id: UserTable.id,
+      name: UserTable.name,
+    });
+};
+
+const sampleDelete = async () => {
+  const user = await db.delete(UserTable).where(eq(UserTable.age, 30)).returning({
+    id: UserTable.id,
+    name: UserTable.name,
+  });
+};
+
 async function main() {
   // sampleInsert();
+  // sampleUpdate();
+  // sampleDelete();
 
   // await db.insert(UserPreferenceTable).values({
   //   emailUpdates: true,
   //   userId: '25fcfa19-a90b-434f-9090-b25d49442161'
   // })
 
+  const usersSelectSql = await db
+    .select({
+      name: UserTable.name,
+      count: count(UserTable.age),
+    })
+    .from(UserTable)
+    .groupBy(UserTable.name)
+    .having((columns) => gt(columns.count, 0));
+
+  const usersSql = await db
+    .select({
+      id: UserTable.id,
+      age: UserTable.age,
+      emailUpdates: UserPreferenceTable.emailUpdates,
+    })
+    .from(UserTable)
+    .where(eq(UserTable.age, 26))
+    .leftJoin(UserPreferenceTable, eq(UserPreferenceTable.userId, UserTable.id));
+
   const users = await db.query.UserTable.findMany({
     columns: { email: false },
     extras: { lowerCaseName: sql<string>`lower(${UserTable.name})`.as("lowerCaseName") },
-    // limit: 1,
-    // offset: 1,
-    with: { preference: true },
+    with: { preference: true, posts: true },
+    // orderBy: asc(UserTable.age)
+    orderBy: (table, { asc }) => asc(table.name),
+    where: (table, func) => func.gt(table.age, 24),
+    // where: (table, func) => func.between(table.age, 24, 26),
   });
+
+  // const usersWithPreferencesEmail = await db.query.UserTable.findMany({
+  //   columns: { email: false },
+  //   extras: { lowerCaseName: sql<string>`lower(${UserTable.name})`.as("lowerCaseName") },
+  //   limit: 1,
+  //   offset: 1,
+  //   with: { preference: {
+  //     columns: {
+  //       emailUpdates: true
+  //     }
+  //   } },
+  // });
   console.log(users);
 }
 

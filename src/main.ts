a7b138a0e2f1f820d170e66654cf1e5ac8c4c42f
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-09-09 10:36:43 +0800

    added sample inserts and queries

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,48 @@
 import "dotenv/config";
 import { db } from "./drizzle/db";
 import { UserTable } from "./drizzle/schema";
+import { sql } from "drizzle-orm";
 
-async function main() {
-  // await db.insert(UserTable).values({
-  //   name: "Ken",
+const sampleInsert = async () => {
+  // await db.delete(UserTable);
+  const user = await db
+    .insert(UserTable)
+    .values([
+      {
+        name: "Ken",
+        age: 24,
+        email: "sample@email.com",
+      },
+      {
+        name: "Rey",
+        age: 25,
+        email: "sample2@email.com",
+      },
+      {
+        name: "Mozo",
+        age: 26,
+        email: "sample3@email.com",
+      },
+    ])
+    .returning({
+      id: UserTable.id,
+      name: UserTable.name,
+    });
+  // .onConflictDoUpdate({
+  //   target: UserTable.email,
+  //   set: { name: 'Updated Name'}
   // });
 
-  // const user = await db.query.UserTable.findFirst();
-  // console.log(user);
+  console.log(user);
+};
 
-  await db.delete(UserTable);
+async function main() {
+  // sampleInsert();
+  const users = await db.query.UserTable.findMany({
+    columns: { email: false },
+    extras: { lowerCaseName: sql<string>`lower(${UserTable.name})`.as("lowerCaseName") },
+  });
+  console.log(users);
 }
 
 main();
